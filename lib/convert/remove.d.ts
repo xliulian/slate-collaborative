3e7eebe2e8f5c2891e8e24466099eb5adf893622
Ulion <ulion2002@gmail.com>
2020-12-29 18:19:18 +0800

    gitpkg

---
+++ lib/convert/remove.d.ts
@@ -0,0 +1,4 @@
+import * as Automerge from 'automerge';
+declare const opRemove: (op: Automerge.Diff, [map, ops]: any) => any[];
+export default opRemove;
+//# sourceMappingURL=remove.d.ts.map
---
+++ lib/cursor/index.d.ts
@@ -0,0 +1,4 @@
+import { Operation, Range } from 'slate';
+import { CursorData } from '../model';
+export declare const setCursor: (id: string, selection: Range | null, doc: any, operations: Operation[], cursorData: CursorData) => any;
+//# sourceMappingURL=index.d.ts.map
