bacada3fdb7b0727768e39ec73881c329b2be6d4
Ulion <ulion2002@gmail.com>
2020-12-29 18:00:52 +0800

    gitpkg

---
+++ lib/convert/insert.d.ts
@@ -0,0 +1,5 @@
+import * as Automerge from 'automerge';
+import { SyncDoc } from '../model';
+declare const opInsert: (op: Automerge.Diff, [map, ops]: any, doc: SyncDoc) => any[];
+export default opInsert;
+//# sourceMappingURL=insert.d.ts.map
---
+++ lib/convert/set.d.ts
@@ -0,0 +1,4 @@
+import * as Automerge from 'automerge';
+declare const opSet: (op: Automerge.Diff, [map, ops]: any, doc: any) => any[];
+export default opSet;
+//# sourceMappingURL=set.d.ts.map
