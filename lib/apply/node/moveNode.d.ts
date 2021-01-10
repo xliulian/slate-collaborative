cef046c84f68e3d018c993480f74c46d310a2547
Ulion <ulion2002@gmail.com>
2021-01-10 17:22:45 +0800

    gitpkg

---
+++ lib/apply/node/moveNode.d.ts
@@ -0,0 +1,5 @@
+import { MoveNodeOperation } from 'slate';
+import { SyncValue } from '../../model';
+declare const moveNode: (doc: SyncValue, op: MoveNodeOperation) => SyncValue;
+export default moveNode;
+//# sourceMappingURL=moveNode.d.ts.map
