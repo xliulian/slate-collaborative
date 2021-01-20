a77626cfc7e2ede109db2326691dff45d77b300c
Ulion <ulion2002@gmail.com>
2021-01-20 09:47:34 +0800

    gitpkg

---
+++ lib/convert/index.d.ts
@@ -0,0 +1,5 @@
+import * as Automerge from 'automerge';
+import { SyncDoc } from '../model';
+declare const toSlateOp: (ops: Automerge.Diff[], doc: SyncDoc) => any[];
+export { toSlateOp };
+//# sourceMappingURL=index.d.ts.map
