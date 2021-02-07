a77a7d1161fbc033ef2339b6a376934d5f266c9b
Ulion <ulion2002@gmail.com>
2021-02-07 13:38:15 +0800

    gitpkg

---
+++ lib/apply/node/splitNode.d.ts
@@ -0,0 +1,5 @@
+import { SplitNodeOperation } from 'slate';
+import { SyncValue } from '../../model';
+declare const splitNode: (doc: SyncValue, op: SplitNodeOperation) => SyncValue;
+export default splitNode;
+//# sourceMappingURL=splitNode.d.ts.map
