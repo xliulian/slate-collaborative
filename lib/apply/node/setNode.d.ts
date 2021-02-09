1cb5cd5f8e52868e82c6201b7a5262230893049b
Ulion <ulion2002@gmail.com>
2021-02-09 18:58:34 +0800

    gitpkg

---
+++ lib/apply/node/setNode.d.ts
@@ -0,0 +1,5 @@
+import { SetNodeOperation } from 'slate';
+import { SyncValue } from '../../model';
+declare const setNode: (doc: SyncValue, op: SetNodeOperation) => SyncValue;
+export default setNode;
+//# sourceMappingURL=setNode.d.ts.map
---
+++ lib/convert/insert.d.ts
@@ -0,0 +1,6 @@
+import * as Automerge from 'automerge';
+import { Element } from 'slate';
+import { SyncDoc } from '../model';
+declare const opInsert: (op: Automerge.Diff, [map, ops]: any, doc: SyncDoc, tmpDoc: Element) => any[];
+export default opInsert;
+//# sourceMappingURL=insert.d.ts.map
