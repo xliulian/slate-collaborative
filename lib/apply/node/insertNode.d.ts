fc7d5c01d1a5aef8f04ff50579534cb7157a48ff
Ulion <ulion2002@gmail.com>
2021-03-01 16:02:00 +0800

    gitpkg

---
+++ lib/apply/node/insertNode.d.ts
@@ -0,0 +1,5 @@
+import { InsertNodeOperation } from 'slate';
+import { SyncValue } from '../../model';
+declare const insertNode: (doc: SyncValue, op: InsertNodeOperation) => SyncValue;
+export default insertNode;
+//# sourceMappingURL=insertNode.d.ts.map
