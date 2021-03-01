c730933173aca6899fd1bd525c1e346a7ab83716
Ulion <ulion2002@gmail.com>
2021-03-01 14:37:44 +0800

    get ready for slate 0.60.3

--- packages/bridge/src/apply/node/splitNode.ts
+++ packages/bridge/src/apply/node/splitNode.ts
@@ -1,25 +1,26 @@
+import Automerge from 'automerge'
 import { SplitNodeOperation } from 'slate'
 
 import { SyncValue } from '../../model'
 import { getParent, getChildren } from '../../path'
-import { cloneNode } from '../../utils'
 
 const splitNode = (doc: SyncValue, op: SplitNodeOperation): SyncValue => {
   const [parent, index]: [any, number] = getParent(doc, op.path)
 
   const target = getChildren(parent)[index]
   const inject = {
-    ...cloneNode(target),
     ...op.properties
   }
 
   if (target.text) {
+    inject.text = new Automerge.Text(target.text.toString().slice(op.position))
     target.text.length > op.position &&
       target.text.deleteAt(op.position, target.text.length - op.position)
-    op.position && inject.text.deleteAt(0, op.position)
   } else {
-    target.children.splice(op.position, target.children.length - op.position)
-    op.position && inject.children.splice(0, op.position)
+    inject.children = target.children.splice(
+      op.position,
+      target.children.length - op.position
+    )
   }
 
   getChildren(parent).insertAt(index + 1, inject)
