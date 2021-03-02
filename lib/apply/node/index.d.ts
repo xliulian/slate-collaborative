80a7fbf97a0a78b5e98ed91007ba0dd552fbfc48
Ulion <ulion2002@gmail.com>
2021-03-02 11:32:15 +0800

    gitpkg

---
+++ lib/apply/node/index.d.ts
@@ -0,0 +1,10 @@
+declare const _default: {
+    insert_node: (doc: import("../..").SyncValue, op: import("slate").InsertNodeOperation) => import("../..").SyncValue;
+    merge_node: (doc: import("../..").SyncValue, op: import("slate").MergeNodeOperation) => import("../..").SyncValue;
+    move_node: (doc: import("../..").SyncValue, op: import("slate").MoveNodeOperation) => import("../..").SyncValue;
+    remove_node: (doc: import("../..").SyncValue, op: import("slate").RemoveNodeOperation) => import("../..").SyncValue;
+    set_node: (doc: import("../..").SyncValue, op: import("slate").SetNodeOperation) => import("../..").SyncValue;
+    split_node: (doc: import("../..").SyncValue, op: import("slate").SplitNodeOperation) => import("../..").SyncValue;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
